af0ef7b30dfb428dc3439435df2006029ffacbdc
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-08-03 11:17:38 +0700

    fix: add user id into payments table

--- database/migrations/2022_08_03_031811_create_payments_table.php
+++ database/migrations/2022_08_03_031811_create_payments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('project_id')->constrained();
             $table->string('photo_url');
             $table->decimal('payment_amount', 10);
