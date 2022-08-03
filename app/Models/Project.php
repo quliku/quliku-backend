5681615af6c795e617debbb0677cfc82165d4fee
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-08-03 11:38:02 +0700

    feat: add project model

---
+++ app/Models/Project.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'contractor_id',
+        'foreman_id',
+        'name',
+        'description',
+        'status',
+        'start_date',
+        'end_date',
+        'province',
+        'city',
+        'district',
+        'village',
+        'address',
+        'project_area',
+        'payment_type',
+    ];
+
+    public function contractor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'contractor_id');
+    }
+
+    public function foreman(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'foreman_id');
+    }
+
+    public function reports(): HasMany
+    {
+        return $this->hasMany(Report::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
