f6e21391733de88d65f84e127c7bb6c7ce59a403
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-08-04 08:17:20 +0700

    feat: add rating table

---
+++ database/migrations/2022_08_04_004519_create_rating_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('rating', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contractor_id')->constrained('users');
+            $table->foreignId('foreman_id')->constrained('users');
+            $table->foreignId('project_id')->constrained();
+            $table->integer('rating');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rating');
+    }
+};
