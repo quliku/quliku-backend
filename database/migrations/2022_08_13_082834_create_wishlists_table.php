0c0fe8181baf649dcda88c12f93289c31d24af96
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-08-13 12:08:19 +0700

    fix: remove timestamp from wishlists table

--- database/migrations/2022_08_13_082834_create_wishlists_table.php
+++ database/migrations/2022_08_13_082834_create_wishlists_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('contractor_id')->constrained('users');
             $table->foreignId('foreman_id')->constrained('users');
-            $table->timestamps();
         });
     }
 
