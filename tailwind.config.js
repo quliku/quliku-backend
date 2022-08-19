dccda87c7c6543ecd8090c7cf00e9223cf8ea41b
husinassegaff <husinassegaff15@gmail.com>
2022-08-19 23:25:53 +0700

    feat: customize colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
+    colors: {
+      'orange': {
+        primary: '#F98214',
+        secondary: '#FBA85B'
+      },
+      'black': {
+        primary: '#424242',
+        secondary: '#8E8E8E'
+      },
+      'white': {
+        primary: '#FFFFFF',
+        secondary: '#F3F3F3'
+      },
+      'green': {
+        primary: '#7BBB71',
+        secondary: '#EAFFE7'
+      },
+      'red': {
+        primary: '#A91325',
+        secondary: '#DB324D'
+      }
+    },
     extend: {},
   },
   plugins: [],
